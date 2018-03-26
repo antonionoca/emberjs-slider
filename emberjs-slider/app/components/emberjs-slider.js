9059c7d1bb017290c49c29bf0a94958368f993c8
Antonio Noca <antonionoca@gmail.com>
2018-03-26 21:38:55 +0100

    onInput action logs to console.

--- emberjs-slider/app/components/emberjs-slider.js
+++ emberjs-slider/app/components/emberjs-slider.js
@@ -3,5 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
   min: 0,
   max: 10,
-  startValue: 5
+  startValue: 5,
+  actions: {
+    onInput() {
+      console.log('onInput');
+    }
+  }
 });
